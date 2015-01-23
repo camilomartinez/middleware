3a28f72da76e999096f10dc6567ccc88b5f11465
camilomartinez <marscamand@gmail.com>
2015-01-23 11:08:26 +0100

    OpenMPI Check rank and size

--- hello/hello.c
+++ hello/hello.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+	int rank, size;
+	
 	MPI_Init(&argc, &argv);
-	printf("Hello, world!\n");
+	
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	printf("Hello, world! ");
+	printf("I am process %d out of %d\n", rank, size);
+	
 	MPI_Finalize();
 	return 0;
 }
