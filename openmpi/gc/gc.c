b4d8e088a2b02603a20d9b4f6d8b09b80a1cc16c
camilomartinez <marscamand@gmail.com>
2015-02-02 15:49:24 +0100

    Reading pgm file

--- openmpi/gc/gc.c
+++ openmpi/gc/gc.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	// Just read the file
+	const char* SPACE = " ";	
 	FILE *fp;
 	char buffer[100];
-
+	char* token;
+	unsigned int width, height, maxvalue;
+	
 	fp = fopen("feep.pgm", "r");
+	// Confirms first line is pgm format
+	fgets(buffer, sizeof(buffer), fp);
+	if(buffer[0] != 'P' || buffer[1] != '2'){
+		exit(1);
+	}
+	fgets(buffer, sizeof(buffer), fp);
+	if(buffer[0] == '#'){
+		// Skip second line if comment	
+		fgets(buffer, sizeof(buffer), fp);	
+	}
+	// Get dimensions
+	token = strtok(buffer, SPACE);
+	width = atoi(token);
+	token = strtok(NULL, SPACE);
+	height = atoi(token);
+	printf("width: %d, height: %d\n", width, height);	
+	// Get maxvalue
+	fgets(buffer, sizeof(buffer), fp);
+	maxvalue = strtok(buffer, SPACE);
+
 	while (fgets(buffer, sizeof(buffer), fp)) {
 		printf("%s",buffer);
+		token = strtok(buffer, SPACE);
+		while (token) {
+    	//	printf("token: %s\n", token);
+    		token = strtok(NULL, SPACE);
+		}		
 	}
 	fclose(fp);
-}
+} 
