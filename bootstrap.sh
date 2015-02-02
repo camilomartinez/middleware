5665542db5aab872ec090907fc2836eb6aabd03e
camilomartinez <marscamand@gmail.com>
2015-02-02 22:48:44 +0100

    Add make file and ignore gc binary

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,6 @@
 
 apt-get update
 apt-get install -y openmpi-bin openmpi-common openssh-client openssh-server libopenmpi1.3 libopenmpi-dbg libopenmpi-dev
+# Useful dev tools
 apt-get install gdb -y
+apt-get install make -y
