bedbebf504364b25ef53661408c7a83bd1ba0a00
camilomartinez <marscamand@gmail.com>
2015-02-06 18:40:24 +0100

    Include logs and ignore them on git

--- openmpi/gc/run.sh
+++ openmpi/gc/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mpirun --mca btl_tcp_if_include eth1 -np 4 --host mpi-node-1,mpi-node-2 gc in_big_feep.pgm
+mpirun --mca btl_tcp_if_include eth1 -np 4 --host mpi-node-1,mpi-node-2 -output-filename log gc in_feep.pgm 
