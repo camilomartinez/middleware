e387c76a497b2ede13467c99faed648c922e40ab
camilomartinez <marscamand@gmail.com>
2015-02-09 19:24:56 +0100

    Base maven new project

---
+++ hadoop/eclipse/LogAnalysis/LogAnalysis/src/main/java/polimi/camilo/hadoop/LogAnalysis/App.java
@@ -0,0 +1,13 @@
+package polimi.camilo.hadoop.LogAnalysis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
