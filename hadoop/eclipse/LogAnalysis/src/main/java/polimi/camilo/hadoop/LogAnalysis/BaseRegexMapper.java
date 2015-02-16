e9b8a4781ddabf3c84c4fa32e99f4c205d9c8b5b
camilomartinez <marscamand@gmail.com>
2015-02-16 12:22:39 +0100

    Refactor regex mapper to use it for the other tasks

---
+++ hadoop/eclipse/LogAnalysis/src/main/java/polimi/camilo/hadoop/LogAnalysis/BaseRegexMapper.java
@@ -0,0 +1,54 @@
+package polimi.camilo.hadoop.LogAnalysis;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public abstract class BaseRegexMapper<T> extends Mapper<LongWritable, Text, Text, T> {
+	
+	private final Text textOutput = new Text();
+	private final LongWritable oneWritable = new LongWritable(1);
+	
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		
+		String logEntry = value.toString();
+		Pattern pattern = Pattern.compile(LogPattern());
+	    Matcher matcher = pattern.matcher(logEntry);
+	    // Pattern doesn't match
+	    if (!matcher.matches()) {
+	    	System.err.println("Omitting entry:");
+			System.err.println(logEntry);
+			return;
+		}
+		if (NumberOfFields() != matcher.groupCount()) {
+			System.err.println("Bad log entry (or problem with RE?):");
+			System.err.println(logEntry);
+			return;
+		}
+	    Text outputKey = MapperOutputKey(matcher);
+	    T outputValue = MapperOutputValue(matcher);
+	    context.write(outputKey, outputValue);
+	}
+	
+	protected abstract String LogPattern();
+	protected abstract int NumberOfFields();
+	protected abstract Text MapperOutputKey(Matcher matcher);
+	protected abstract T MapperOutputValue(Matcher matcher);
+	
+	// Some base implementations
+	protected Text OutputGroup(Matcher matcher, int groupNumber) {
+		String groupMatch = matcher.group(groupNumber);
+	    textOutput.set(groupMatch);
+	    return textOutput;
+	}
+	
+	protected LongWritable OutputOne() {
+		return oneWritable;
+	}
+}
--- hadoop/eclipse/LogAnalysis/src/main/java/polimi/camilo/hadoop/LogAnalysis/PageviewsMapper.java
+++ hadoop/eclipse/LogAnalysis/src/main/java/polimi/camilo/hadoop/LogAnalysis/PageviewsMapper.java
@@ -1,45 +1,36 @@
 package polimi.camilo.hadoop.LogAnalysis;
 
-import java.io.IOException;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
 
-public class PageviewsMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
-	private final String LogPattern = "^([\\d.]+) \\S+ \\S+ \\[([\\w/]+):([\\w:]+\\s[+\\-]\\d{4})\\] \"(.+?html\\s.+?)\" (\\d{3}) (\\d+) \"([^\"]+)\" \"([^\"]+)\"";
-	private int NUM_FIELDS = 8;
-	
-	private final LongWritable one = new LongWritable(1);
-	private Text dateText = new Text();
+public class PageviewsMapper extends BaseRegexMapper<LongWritable> {
+	private final String logPattern = "^([\\d.]+) \\S+ \\S+ \\[([\\w/]+):([\\w:]+\\s[+\\-]\\d{4})\\] \"(.+?html\\s.+?)\" (\\d{3}) (\\d+) \"([^\"]+)\" \"([^\"]+)\"";
+	private int numFields = 8;
 	
 	public PageviewsMapper()  {
 		System.out.println("Init Pageviews Mapper");
 		}
 
 	@Override
-	protected void map(LongWritable key, Text value, Context context)
-			throws IOException, InterruptedException {
-		
-		String logEntry = value.toString();
-		Pattern pattern = Pattern.compile(LogPattern);
-	    Matcher matcher = pattern.matcher(logEntry);
-	    // Pattern matching error
-	    if (!matcher.matches()) {
-	    	System.err.println("Omitting entry:");
-			System.err.println(logEntry);
-			return;
-		}
-		if (NUM_FIELDS != matcher.groupCount()) {
-			System.err.println("Bad log entry (or problem with RE?):");
-			System.err.println(logEntry);
-			return;
-		}
-	    String dateEntry = matcher.group(2);
-	    dateText.set(dateEntry);
-	    context.write(dateText, one);
+	protected String LogPattern() {
+		return logPattern;
+	}
+
+	@Override
+	protected int NumberOfFields() {
+		return numFields;
+	}
+
+	@Override
+	protected Text MapperOutputKey(Matcher matcher) {
+		return OutputGroup(matcher, 2);
+	}
+
+	@Override
+	protected LongWritable MapperOutputValue(Matcher matcher) {
+		return OutputOne();
 	}
 
 }
