ad7540dbb5791d418e93147b69be54c095f3271e
camilomartinez <marscamand@gmail.com>
2015-02-19 16:43:02 +0100

    Run openmpi with 4 nodes

--- openmpi/gc/run.sh
+++ openmpi/gc/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # eth1 must be the public network interface if there are communications issues check this
-time mpirun --mca btl_tcp_if_include eth1 -np 2 --host mpi-node-1,mpi-node-2 -output-filename log gc in_feep.pgm 
+time mpirun --mca btl_tcp_if_include eth1 -np 4 --host mpi-node-1,mpi-node-2,mpi-node-3,mpi-node-4 -output-filename log gc in_feep.pgm 
